bb17d619f6cab5cab833aca540d3856da1e05d6c
Edwin Castellanos <gnuedwin@gmail.com>
2022-07-14 01:20:23 -0400

    fixed modificar & eliminar

--- administrador/seccion/productos.php
+++ administrador/seccion/productos.php
@@ -37,12 +37,27 @@ switch($accion){
         if($txtImagen!=""){ 
             $fecha = new DateTime();
             $nombreArchivo=($txtImagen!="")?$fecha->getTimestamp()."_".$_FILES["$txtImagen"]["name"]:"imagen.jpg"; 
+            $tmpImagen=$_FILES["txtImagen"]["tmp_name"]; 
 
-            $tmpImagen=$_FILES["txtImagen"]["tmp_name"];
+            move_uploaded_file($tmpImagen,"../../img/".$nombreArchivo); 
+
+            
+            // para evitar que se dupliquen las imagenes si tienen el mismo nombre
+            $sentenciaSQL= $conexion->prepare("SELECT imagen FROM libros2 WHERE id=id"); 
+            $sentenciaSQL->bindParam(':id',$txtID); 
+            $sentenciaSQL->execute(); 
+            $libro=$sentenciaSQL->fetch(PDO::FETCH_LAZY);    
+        
+            if(isset($libro["imagen"])&&($libro["imagen"]!="imagen.jpg")){ 
+                if(file_exists("../../img/".$libro["imagen"])){
+                    unlink("../../img/".$libro["imagen"]);
+            }
+
+        }
 
 
             $sentenciaSQL= $conexion->prepare("UPDATE libros2 SET imagen=:imagen WHERE id=:id"); 
-            $sentenciaSQL->bindParam(':imagen',$txtImagen); 
+            $sentenciaSQL->bindParam(':imagen',$nombreArchivo); 
             $sentenciaSQL->bindParam(':id',$txtID); 
             $sentenciaSQL->execute(); 
     }
